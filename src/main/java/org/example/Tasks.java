34723c47ff18d304df362f16502d11c719c19bba
vladbooker <vladislav.babenya@gmail.com>
2024-07-26 00:51:08 +0300

    Lesson 1

---
+++ src/main/java/org/example/Tasks.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Tasks {
+
+    public static void printThreeWorlds() {
+    System.out.println("Orange");
+    System.out.println("Banana");
+    System.out.println("Apple");
+}
+}
